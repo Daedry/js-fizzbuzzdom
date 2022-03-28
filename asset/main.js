3279942c085c7e64ada25cea0520eaa7d7222b99
MyUsername <romamargine@gmail.com>
2022-03-28 15:49:50 +0200

    add es

--- asset/main.js
+++ asset/main.js
@@ -1,9 +1,24 @@
 //Scrivi un programma che stampi in console i numeri da 1 a 100.
 
-
 /* 
 MILESTONE 1
 Per i multipli di 3 stampi “Fizz” al posto del numero
 Per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 */
+
+for (var i = 1; i <= 100; i++) {
+
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("FizzBuzz");
+  
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+  
+  } else {
+    console.log(i);
+  }
+}
